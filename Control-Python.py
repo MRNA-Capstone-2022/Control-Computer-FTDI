bae2fbaa266f2f7d8ed5f5761a1448b2fb1dbbc0
jdl203 <jfl5677@psu.edu>
2022-02-25 14:17:10 -0500

    Completed first Iteration of PID
    Rotors remain at same speed, slightly off on angles (~5 degrees)

--- Control-Python.py
+++ Control-Python.py
@@ -3,10 +3,10 @@ import time
 import math
 
 # Setup com ports (Subject to change based on where USB is plugged in)
-com1 = iq.SerialCommunicator("/dev/cu.usbserial-14410")
-com2 = iq.SerialCommunicator("/dev/cu.usbserial-14420")
-com3 = iq.SerialCommunicator("/dev/cu.usbserial-14430")
-com4 = iq.SerialCommunicator("/dev/cu.usbserial-14440")
+com1 = iq.SerialCommunicator("/dev/cu.usbserial-14110")
+com2 = iq.SerialCommunicator("/dev/cu.usbserial-14120")
+com3 = iq.SerialCommunicator("/dev/cu.usbserial-14130")
+com4 = iq.SerialCommunicator("/dev/cu.usbserial-14140")
 
 # VARIABLES
 elapsedTime = 0.0, time = 0.0, timePrev = 0.0
@@ -41,17 +41,75 @@ motor4 = iq.Vertiq2306(com4, 0)
 motors = [motor1, motor2, motor3, motor4]
 
 # Store initial time
-time = time.time()
+startTime = time.time()
 
 # Set initial velocity
 for motor in motors:
     motor.set("propeller_motor_control", "ctrl_velocity", motorRads)
 
-def PID_Function(serialPort, motorRads, pid_p, pid_i, pid_d, prevPID, error, motorTime, est_angle, offset):
+def hmodRadf(h):
+    dh = 0.0
+    i = 0
 
-    desired_angle = 
+    if (h > 0):
+        i = int(h/(2*math.pi)+0.5)
+    else:
+        i = int(h/(2*math.pi)-0.5)
+    dh = h - math.pi*2*i
 
+    return dh
 
+def PID_Function(motor, motorRads, pid_p, pid_i, pid_d, prevPID, error, motorTime, est_angle, offset):
+    # get the angle of the virtual reference
+    desired_angle = hmodRadf(motorRads*(time/1000))
+
+    # get the angle of actual motor
+    currentAngle = motor.get("brushless_drive", "obs_angle")
+
+    # find the difference between motor and "ref"
+    # offset 0: motor is parallel to the virtual motor
+    # offset PI/2: motor is 90 degrees off with respect to virtual motor
+    diff = hmodRadf(desired_angle - angle + offset); 
+
+    # new estimate angle
+    """ 1. propogate 
+    //      angle += (measured rpm)*elapsedTime
+    // 2. update  
+    //      estimated diff = estimated angle - desired angle
+    // update = ((diff-est_diff)->[-PI to PI])*((small_gain)->[0-1]))
+    // est_diff += update
+    // angle -= update """
+    currentVelocity = motor.get("brushless_drive", "obs_velocity")
+    est_angle += currentVelocity*motorTime               # estimate the angle of the motor using time since last PID call
+    est_angle = hmodRadf(est_angle)                      # set est_angle between [-Pi, Pi]
+  
+    est_diff = desired_angle - est_angle + offset   # estimate the difference between the desired and estimated angle
+    est_diff = hmodRadf(est_diff)                       # set est_diff between [-Pi, Pi]
+  
+    updateangle = hmodRadf(diff-est_diff)*0.1
+    est_diff += updateangle
+    est_angle -= updateangle
+    *est_angle = hmodRadf(*est_angle);
+
+    // Calculating P, I, D
+    *pid_p = est_diff*kp;
+    *pid_i += est_diff*(*motortime);
+    //*pid_d = (motorRads - obs_vel)*kd; //not in use
+
+    // Final PID 
+    PID = *pid_p + ki*(*pid_i) + *pid_d;
+    
+    // Calculate the new velocity
+    newvel = motorRads*1.04 + PID;  // gain value 1.04 puts real motor speed closest to the virtual motor
+    
+    // Command new velocity to motor
+    serialPort.set(prop.ctrl_velocity_,newvel);
+    
+    // Save current PID value for future PID
+    *prevPID = PID;
+    
+    // Save current error for future PID
+    *error = est_diff;
 
 
 
